2d4e2fe3e8fd78536bb9f8ad7dd982b7011ae6e1
Edmar Paulino <edmarpaulino9@gmail.com>
2021-07-13 10:45:23 -0300

    new base tests for the main function

--- c07/ex04/main.c
+++ c07/ex04/main.c
@@ -176,5 +176,47 @@ int	main(void)
 	free(nbr_converted);
 
 	printf("-----------------------------------\n");
+	printf("%s\n", "\nHEX TO BIN:");
+
+	input_base = "0123456789abcdef";
+	output_base = "01";
+
+	nbr_converted = ft_convert_base("\t\n\v\f\r +-+--0", input_base, output_base);
+	printf("-----------------------------------\n");
+	printf("nbr = \\t\\n\\v\\f\\r +-+--0\ninput base = %s\noutput base = %s\nnbr converted = %s\n", input_base, output_base, nbr_converted);
+	free(nbr_converted);
+
+	nbr_converted = ft_convert_base("\t\n\v\f\r +-+--80000000", input_base, output_base);
+	printf("-----------------------------------\n");
+	printf("nbr = \\t\\n\\v\\f\\r +-+--80000000\ninput base = %s\noutput base = %s\nnbr converted = %s\n", input_base, output_base, nbr_converted);
+	free(nbr_converted);
+
+	nbr_converted = ft_convert_base("\t\n\v\f\r +-+-7fffffff", input_base, output_base);
+	printf("-----------------------------------\n");
+	printf("nbr = \\t\\n\\v\\f\\r +-+-7fffffff\ninput base = %s\noutput base = %s\nnbr converted = %s\n", input_base, output_base, nbr_converted);
+	free(nbr_converted);
+
+	printf("-----------------------------------\n");
+	printf("%s\n", "\nBIN TO HEX:");
+
+	input_base = "01";
+	output_base = "0123456789abcdef";
+
+	nbr_converted = ft_convert_base("\t\n\v\f\r +-+--0", input_base, output_base);
+	printf("-----------------------------------\n");
+	printf("nbr = \\t\\n\\v\\f\\r +-+--0\ninput base = %s\noutput base = %s\nnbr converted = %s\n", input_base, output_base, nbr_converted);
+	free(nbr_converted);
+
+	nbr_converted = ft_convert_base("\t\n\v\f\r +-+---10000000000000000000000000000000", input_base, output_base);
+	printf("-----------------------------------\n");
+	printf("nbr = \\t\\n\\v\\f\\r +-+--10000000000000000000000000000000\ninput base = %s\noutput base = %s\nnbr converted = %s\n", input_base, output_base, nbr_converted);
+	free(nbr_converted);
+
+	nbr_converted = ft_convert_base("\t\n\v\f\r +-+-1111111111111111111111111111111", input_base, output_base);
+	printf("-----------------------------------\n");
+	printf("nbr = \\t\\n\\v\\f\\r +-+-1111111111111111111111111111111\ninput base = %s\noutput base = %s\nnbr converted = %s\n", input_base, output_base, nbr_converted);
+	free(nbr_converted);
+
+	printf("-----------------------------------\n");
 	return (0);
 }
